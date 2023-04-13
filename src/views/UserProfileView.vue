f7a21e68439b103c4a0698e2a14072be1d5195d3
Maxime Authié <authie.maxime@orange.fr>
2023-04-13 19:42:15 +0200

    Ajour de la logique de verif des données

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -10,25 +10,31 @@
 
             <div class="form">
                 <ion-label position="fixed">Prénom</ion-label>
-                <ion-input class="custom" type="text" name="firstName"></ion-input>
+                <ion-input v-model="userData.firstName" @keyup="checkImputKeyUp" :class="{badInput: isFirstNameEmpty}" type="text" name="firstName"></ion-input>
+                <p v-if="isFirstNameEmpty" class="errorMsg errorMsgImput">Veuillez saisir votre prénom</p>
 
                 <ion-label position="fixed">Nom</ion-label>
-                <ion-input class="custom" type="text" name="lastName"></ion-input>
-
+                <ion-input v-model="userData.lastName" @keyup="checkImputKeyUp" :class="{badInput: isLastNameEmpty}" type="text" name="lastName"></ion-input>
+                <p v-if="isLastNameEmpty" class="errorMsg errorMsgImput">Veuillez saisir votre nom</p>
+                
                 <ion-label position="fixed">Surnom</ion-label>
-                <ion-input class="custom" type="text" name="nickName"></ion-input>
+                <ion-input v-model="userData.nickName" class="custom" type="text" name="nickName"></ion-input>
             
                 <ion-label position="fixed">Adresse mail</ion-label>
-                <ion-input class="custom" type="email" name="mail"></ion-input>
+                <ion-input v-model="userData.mail" @keyup="checkImputKeyUp" :class="{badInput: isMailEmpty}" type="email" name="mail"></ion-input>
+                <p v-if="isMailEmpty" class="errorMsg errorMsgImput">Veuillez saisir votre adresse mail</p>
+                <p v-if="!isMailCorrect" class="errorMsg errorMsgImput">Adresse mail incorrecte.</p>
 
-                <ion-label position="fixed">Mot de passe</ion-label>
-                <ion-input class="custom" type="password" name="password1"></ion-input>
+                <ion-label position="fixed">Nouveau mot de passe</ion-label>
+                <ion-input v-model="userData.password" @keyup="checkImputKeyUp" :class="{badInput: passwordError}" type="password" name="password"></ion-input>
             
-                <ion-label position="fixed">Ressaisir le mot de passe</ion-label>
-                <ion-input class="custom" type="password" name="password2"></ion-input>
+                <ion-label position="fixed">Ressaisir le nouveau mot de passe</ion-label>
+                <ion-input v-model="userData.rePassword" @keyup="checkImputKeyUp" :class="{badInput: passwordError}" type="password" name="rePassword"></ion-input>
+
+                <p v-if="passwordError" class="errorMsg p-center">Les deux mots de passe saisis ne sont pas identiques!</p>
 
                 <div class="button">
-                    <ion-button class="custom main" expand="block">Enregistrer</ion-button>
+                    <ion-button @click="submitRegister" class="custom main" expand="block">Enregistrer</ion-button>
                     <router-link to="/delete-account" class="no-underline">
                         <ion-button class="custom accessory" expand="block">Supprimer mon compte</ion-button>
                     </router-link>
@@ -49,7 +55,103 @@
     import NavBar from '../components/NavBarComponent.vue';
 
     export default defineComponent({
-        components: { 'nav-bar': NavBar, IonContent, IonInput, IonLabel, IonAvatar, IonButton, IonFooter},
+        name: "user-profile",
+        components: { 'nav-bar': NavBar, IonContent, IonInput, IonLabel, IonAvatar, IonButton, IonFooter },
+        data() {
+            return {
+                isFirstNameEmpty: false,
+                isLastNameEmpty: false,
+                isMailEmpty: false,
+                isPasswordEmpty: true,
+                isRePasswordEmpty: true,
+                isMailCorrect: true,
+                isImputEmpty: true,
+                passwordError: false,
+                userData: {
+                    firstName: 'Maxime',
+                    lastName: 'Authié',
+                    nickName: 'DarkRabbiT',
+                    mail: 'authie.maxime@orange.fr',
+                    password: '',
+                    rePassword:''
+                },
+            }
+        },
+        methods: {
+            submitRegister() {
+                this.checkImputSubmit();
+                this.checkMail();
+                this.checkPassword
+                if (this.isImputEmpty == false && this.isMailCorrect == true && !this.passwordError) {
+                    //this.$router.push('/conversation-list')
+                }
+            },
+            checkImputKeyUp() {
+                if (this.userData.firstName != '') {
+                  this.isMailEmpty= false;
+                }
+                if (this.userData.lastName != '') {
+                  this.isMailEmpty= false;
+                } 
+                if (this.userData.mail != '') {
+                  this.isMailEmpty= false;
+                } 
+                if (this.userData.password != '') {
+                    this.isPasswordEmpty= false;
+                }
+                if (this.userData.rePassword != '') {
+                    this.isRePasswordEmpty= false;
+                } 
+            },
+            checkImputSubmit() { // Vérifie si tous les champs sont remplis
+              this.resetIsEmptyData(); // Remets tous les booléens à leur valeur par défaut
+              if (this.userData.firstName == '') {
+                  this.isFirstNameEmpty= true;
+                  this.isImputEmpty= true;
+              }
+              if (this.userData.lastName == '') {
+                  this.isLastNameEmpty= true;
+                  this.isImputEmpty= true;
+              }
+              if (this.userData.mail == '') {
+                  this.isMailEmpty= true;
+                  this.isImputEmpty= true;
+              }
+              if (this.userData.password != '') {
+                  this.isPasswordEmpty= false;
+              }
+              if (this.userData.rePassword != '') {
+                  this.isRePasswordEmpty= false;
+              }
+          },
+          resetIsEmptyData() { // Remets tous les booléens à false 
+                this.isImputEmpty=false;
+                this.isFirstNameEmpty= false;
+                this.isLastNameEmpty= false;
+                this.isMailEmpty= false;
+                this.isPasswordEmpty= true;
+                this.isRePasswordEmpty= true;
+          },
+          checkPassword() { // Vérifie si les deux password sont identiques
+                this.passwordError = false;
+                if (this.isPasswordEmpty == false && this.isRePasswordEmpty == false) {
+                    if (this.userData.password != this.userData.rePassword) {
+                        this.passwordError= true;
+                    }
+                }
+          },
+          checkMail() { // Vérifie si le format du mail est correct
+            this.isMailCorrect = true;
+            const pattern = /^[a-z0-9.-]{2,}@+[a-z0-9.-]{2,}$/i;
+            if (this.userData.mail != '') {
+              if (pattern.test(this.userData.mail)) {
+              this.isMailCorrect = true;
+              } else {
+                this.isMailCorrect = false;
+              }
+            }
+          }
+        }
     }); 
 </script>
 
@@ -87,12 +189,13 @@
         line-height: 0px;
     }
 
-    ion-input.custom {
+    ion-input {
         --background: var(--ion-color-brutLight);
         height: 40px;
         border: 4px solid black;
         box-shadow: 7px 7px 0px black;
         margin-bottom: 1.5vh;
+        --padding-start: 1vw;
         font-size: 1.2em;
         color: black;
     }
