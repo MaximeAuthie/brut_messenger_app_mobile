1e8d0860c71fc490edb1c909233b47e0d26747e7
Maxime Authié <authie.maxime@orange.fr>
2023-05-02 16:12:09 +0200

    implémentation logique

--- src/views/GroupSettingsView.vue
+++ src/views/GroupSettingsView.vue
@@ -3,8 +3,8 @@
         <ion-grid>
             <ion-row>
                 <ion-col size="2">
-                    <router-link to="/conversation-list">
-                        <router-link to="/conversation-list"><img class="return" src="../../public/assets/icon/back.png" id="return-icon" alt="Retour à la liste des conversations" title="Retour à la liste des conversations"></router-link>
+                    <router-link to="/conversation">
+                        <router-link to="/conversation"><img class="return" src="../../public/assets/icon/back.png" id="return-icon" alt="Retour à la liste des conversations" title="Retour à la liste des conversations"></router-link>
                     </router-link>
                     
                 </ion-col>
@@ -20,7 +20,8 @@
 
         <div class="form">
             <ion-label position="fixed">Nom du groupe</ion-label>
-            <ion-input class="custom" type="text" name="groupName">{{ group.name }}</ion-input>
+            <ion-input v-model="groupData.name" :class="{badInput: isGroupNameEmpty}" type="text" name="groupName"></ion-input>
+            <p v-if="isGroupNameEmpty" class="errorMsg errorMsgImput">Veuillez saisir un nom de groupe</p>
             <br>
             <ion-grid>
                 <ion-row>
@@ -34,14 +35,18 @@
                     </ion-col>
                 </ion-row>
             </ion-grid>
-            <member-button v-for="member in memberList"
+            <member-button v-for="member in groupData.membersList"
                 :key="member.id"
                 :name="member.name"
                 :url-img="member.urlImg"
             >
             </member-button>
+
+            <p v-if="isMembersListEmpty" class="errorMsg errorMsgImput">Veuillez ajouter des membres au groupe</p>
+            <p v-if="isFormSubmit" class="successMsg">Les paramètres du groupe ont été mis à jour</p>
+
             <div class="button">
-                <ion-button class="custom main" expand="block">Enregistrer</ion-button>
+                <ion-button @click="submitGroupSettings" class="custom main" expand="block">Enregistrer</ion-button>
                 <ion-button class="custom accessory" expand="block">Quitter le groupe</ion-button>
             </div>
         </div>
@@ -63,25 +68,67 @@
         },
         data() {
             return {
-                group: {
+                isGroupNameEmpty: false,
+                isMembersListEmpty: false,
+                isImputEmpty: false,
+                isFormSubmit: false,
+                groupData: {
                     id: 1,
-                    name: "La famille <3"
+                    name: "La famille",
+                    membersList: [
+                        {
+                            id:1,
+                            name:'Cécilia Orsi',
+                            urlImg:'../../public/assets/images/cecilia.jpg',
+                            role:'1'
+                        },
+                        {
+                            id:2,
+                            name:'Julia Biderman',
+                            urlImg:'../../public/assets/images/julia.jpg',
+                            role:'2'
+                        },
+                    ],
                 },
-                memberList: [
-                    {
-                        id:1,
-                        name:'Cécilia Orsi',
-                        urlImg:'../../public/assets/images/cecilia.jpg',
-                        role:'1'
-                    },
-                    {
-                        id:2,
-                        name:'Julia Biderman',
-                        urlImg:'../../public/assets/images/julia.jpg',
-                        role:'2'
-                    },
-                ],
             }
+        },
+        watch: {
+            groupData() {
+                if (this.groupData.membersList.length !== 0) {
+                    this.isMembersListEmpty = false;
+                }
+            }
+           
+        },
+        methods: {
+            submitGroupSettings() {
+                this.checkImputSubmit();
+                if (!this.isImputEmpty) {
+                    this.isFormSubmit = true;
+                }
+            },
+            checkImputKeyUp() {
+                if (this.groupData.name != '') {
+                  this.isGroupNameEmpty = false;
+                }
+            },
+            checkImputSubmit() { // Vérifie si tous les champs sont remplis
+              this.resetIsEmptyData(); // Remets tous les booléens à leur valeur par défaut
+                if (this.groupData.name == '') {
+                  this.isGroupNameEmpty = true;
+                  this.isImputEmpty = true;
+                }
+                if (this.groupData.membersList.length == 0) {
+                  this.isMembersListEmpty = true;
+                  this.isImputEmpty = true;
+                }
+            },
+            resetIsEmptyData() { // Remets tous les booléens à false +
+                this.isFormSubmit = false;
+                this.isImputEmpty = false;
+                this.isGroupNameEmpty = false;
+                this.isMembersListEmpty = false;
+            },
         }
     }); 
 </script>
@@ -135,7 +182,7 @@
         line-height: 0px;
     }
 
-    ion-input.custom {
+    ion-input {
         --background: var(--ion-color-brutLight);
         --padding-start:1vw;
         height: 40px;
@@ -191,9 +238,18 @@
         font-size: 1.2em;
         font-family: 'Space Mono', monospace;
     }
-
+    .errorMsgImput {
+        margin-left: 5%;
+    }
     .badInput {
         border: 4px solid #F55A4F;
         box-shadow: 7px 7px 0px black;
+    }
+    .successMsg {
+        color: rgb(62, 118, 87);
+        font-weight: bold;
+        font-size: 1.2em;
+        font-family: 'Space Mono', monospace;
+        text-align: center;
     }
 </style>
